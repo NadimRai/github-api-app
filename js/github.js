a1f5b8b44c42fb1be31ea6f926b21e1201efbf44
NadimRai <rai_nadim@yahoo.com>
2017-02-17 09:32:45 -0800

    added js folder and js files

---
+++ js/github.js
@@ -0,0 +1,33 @@
+function Github() {
+}
+
+Github.prototype.getUser = function(username, displayFunction) {
+  var info = [];
+  var repoInfo = [];
+  
+  $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response) {
+    
+      var user = response.login;
+      var name = response.name;
+      var location = response.location;
+      var image = response.avatar_url;
+      info.push(user, name, location, image);
+
+    
+    $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey).then(function(result) {
+      var repos = result;
+      repos.forEach(function(repo) {
+        var repoName = repo.name;
+        var description = repo.description;
+        var language = repo.language;
+        repoInfo.push([repoName, description, language]);
+      });
+      displayFunction(info, repoInfo);
+    });
+  }).fail(function(error){
+    $('#user-not-found').empty();
+    $('#user-info').empty();
+    $('#user-repos').empty();
+    $('#user-not-foundor').append("Github user can not be found" );
+  });
+};
